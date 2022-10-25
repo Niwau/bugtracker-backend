63ce08bc9eb5986855a5e51c3a2929f2bca337c3
Guilherme Ribeiro <xxguilhermesoaresxx@gmail.com>
2022-10-25 13:04:27 -0300

    Erro resolvido ao mudar de MySQL para Postgres

---
+++ prisma/migrations/20221025160006_changing_database_to_postgres/migration.sql
@@ -0,0 +1,33 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('User', 'Admin');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('Solved', 'High', 'Medium', 'Low');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'User',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Bug" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Bug_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Bug" ADD CONSTRAINT "Bug_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
